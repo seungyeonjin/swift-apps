288674a5869ade4aa59c1bf73999171d180d8997
camillejin <camillejin@yonsei.ac.kr>
2022-08-23 17:47:34 +0900

    H4X0R News

---
+++ Tasks/Tasks/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by 진승연 on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get all current saved tasks
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: <#T##IndexPath#>)
+        return cell
+    }
+}
