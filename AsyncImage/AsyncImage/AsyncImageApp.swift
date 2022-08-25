2fe3e877d46c132aa2b7dc6cda1fcec91c1f11cb
camillejin <camillejin@yonsei.ac.kr>
2022-08-25 20:52:51 +0900

    AsyncImage

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by 진승연 on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
