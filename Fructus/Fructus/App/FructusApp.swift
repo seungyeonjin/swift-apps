8bff75c297bc6c166def384df6996619085f4d5d
camillejin <camillejin@yonsei.ac.kr>
2022-09-03 11:02:08 +0900

    Fructus

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+@main // entry point of the execution
+struct FructusApp: App {
+    
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
+    var body: some Scene {
+        WindowGroup {
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
